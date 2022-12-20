29a3438854d7a004594e54030331fe89d184454d
Edward Edmonds <edward.edmonds@gmail.com>
2022-12-20 10:20:54 -0500

    Updated comments in code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import prompt from 'prompt-sync';
-let question = prompt();
 
-// make a function to return computer choice
+// Wrapper function to make prompt from prompt-sync human readable
+function askUserQuestion(question) {
+  let questionToAsk = prompt();
+  return questionToAsk(question);
+}
+
+// Function to return a random computer choice
 function computerChoice() {
   let computerSelection = Math.floor(Math.random() * 3);
   if (computerSelection === 0) {
@@ -13,13 +18,13 @@ function computerChoice() {
   }
 }
 
-// make a function to return the player choice
+// Function to get the player choice
 function playerChoice() {
-  let playerSelection = question('Choose rock, paper, or scissors: ').toLowerCase();
+  let playerSelection = askUserQuestion('Choose rock, paper, or scissors: ').toLowerCase();
   return playerSelection;
 }
 
-// make a function to return player verses computer choices
+// Function to compare player verses computer choices
 let playerWins = 0;
 let computerWins = 0;
 
@@ -38,18 +43,17 @@ function playerVersusComputer() {
   }
 }
 
-// play round
+// Wrapper sugar
 function playRound() {
   return playerVersusComputer();
 }
 
-// playGame
+// Function for main game loop
 function playGame(howManyRounds) {
   let numberOfRoundsToPlay = howManyRounds;
 
   console.log('---------');
-  console.log('Rock, Paper, Scissors!');
-  console.log(`Number of rounds we are going to play: ${numberOfRoundsToPlay}`);  
+  console.log(`Sweet, we will play ${numberOfRoundsToPlay} rounds!`);  
   console.log('---------');
 
   for (let numberOfRoundsPlayed = 1; numberOfRoundsPlayed <= numberOfRoundsToPlay; numberOfRoundsPlayed++) {
@@ -67,7 +71,7 @@ function playGame(howManyRounds) {
   }
 }
 
-// prompt for how many games we should play then call playGame()
+// Ask the user how many games we should play then call playGame()
 console.log('Welcome to Rock, Paper, Scissors. A game of ESP brute force!');
-let numberOfRounds = question('How many rounds would you like to play? ');
+let numberOfRounds = askUserQuestion('How many rounds would you like to play? ');
 playGame(numberOfRounds);
