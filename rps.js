8e3f8cd6aa3003f231e3f0d619f7bd86f842e7d5
Edward Edmonds <edward.edmonds@gmail.com>
2022-12-22 10:26:39 -0500

    Implement functions to ask for player name and number of rounds to play

--- rps.js
+++ rps.js
@@ -3,6 +3,9 @@ import chalk from 'chalk';
 import boxen from 'boxen';
 
 const scoreCard = {
+  numberOfRoundsToPlay: 0,
+  numberOfRoundsPlayed: 0,
+  playerName: '',
   playerWins: 0,
   computerWins: 0,
   ties: 0,
@@ -20,7 +23,7 @@ function computerChoice() {
 }
 
 async function playerChoice() {
-  let playerItemChoice = {
+  const playerItemChoice = {
     type: 'list',
     name: 'playerItemSelection',
     message: 'What item do you want to pick?',
@@ -39,25 +42,69 @@ async function playerChoice() {
 async function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     scoreCard.ties++;
-    return 'Tie';
+    return chalk.blue('tie!'.toUpperCase());
   } else if (
     (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
     scoreCard.playerWins++;
-    return 'Player Wins';
+    return chalk.green('player wins!'.toUpperCase());
   } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection == 'rock') || (computerSelection === 'scissors' && playerSelection === 'paper')) {
     scoreCard.computerWins++;
-    return 'Computer Wins';
+    return chalk.magenta('computer wins!'.toUpperCase());
   }
+}
+
+function whoWinsTheGame() {
+  if (scoreCard.playerWins > scoreCard.computerWins) {
+    return 'player wins the game!'.toUpperCase();  
+  } else if (scoreCard.playerWins < scoreCard.computerWins) {
+    return 'computer wins the game!'.toUpperCase();
+  } else {
+    return 'tie game!'.toUpperCase();
+  }
+}
+
+async function askHowManyRoundsToPlay() {
+  const howManyRounds = {
+    type: 'number',
+    name: 'numberOfRoundsSelected',
+    message: `How many rounds would you like to play ${scoreCard.playerName}?`,
+    default: 5,
+  }
+
+  await inquirer.prompt(howManyRounds).then((answer) => {
+    scoreCard.numberOfRoundsToPlay = answer.numberOfRoundsSelected;
+  });
+}
+
+async function askForPlayerName() {
+  const whatIsYourName = {
+    type: 'input',
+    name: 'myNameIs',
+    message: 'What is your name?',
+    default: 'player1',
+  }
+  
+  await inquirer.prompt(whatIsYourName).then((answer) => {
+    scoreCard.playerName = answer.myNameIs;
+  });
 }
 
 async function playGame() {
 
   console.log(boxen('Rock! Paper! Scissors!', {padding: 1, borderColor: 'cyan'}));
 
-  playRound(await playerChoice(), computerChoice()).then((winner) => {
-    console.log(winner);
-  });
-  console.log(scoreCard);
+  await askForPlayerName();
+
+  await askHowManyRoundsToPlay();
+
+  while (scoreCard.numberOfRoundsPlayed < scoreCard.numberOfRoundsToPlay) {
+    await playRound(await playerChoice(), computerChoice()).then((whoWon) => {
+      console.log(whoWon);
+    });
+    scoreCard.numberOfRoundsPlayed++;
+  }
+
+  console.log(boxen(whoWinsTheGame(), {padding: 1, borderColor: 'cyan'}));
 }
 
 playGame();
